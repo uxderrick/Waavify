47cc29aeefe746a7a6d356445a37db9b0cb495ee
Derrick Tsorme <49356701+uxderrick@users.noreply.github.com>
2023-10-17 17:25:58 +0000

    made the top artist prominent

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,7 +1,7 @@
 import { Flex, Heading, Text, Button } from "@radix-ui/themes";
 import React from "react";
 
-const MusicCard = ({ trackData }) => {
+const MusicCard = ({ trackData, isFirstCard }) => {
   // handle button click
   const handleButtonClick = () => {
     const urlToOpen = `${trackData?.external_urls?.spotify}`;
@@ -14,7 +14,12 @@ const MusicCard = ({ trackData }) => {
     //url for track image
     //trackData.album.images[0].url
     <>
-      <Flex className="music-card" direction="column" gap="5">
+      <Flex
+        className="music-card"
+        direction="column"
+        gap="5"
+        style={{ backgroundColor: `${isFirstCard ? "gold" : "#272727"}` }}
+      >
         <Flex
           className="music-photo"
           style={{ backgroundImage: `url(${trackData.album.images[0].url})` }}
@@ -26,17 +31,33 @@ const MusicCard = ({ trackData }) => {
           width="100%"
         >
           <Flex direction="column" gap="1" className="no-bg">
-            <Heading className="no-bg song-title" size="4">
+            <Heading
+              className="no-bg song-title"
+              size="4"
+              style={{ color: `${isFirstCard ? "#272727" : "#ffffff"}` }}
+            >
               {trackData?.name}
             </Heading>
             <Flex gap="2" className="no-bg" justify="center">
-              <Text className="song-title small-text no-bg" size="2">
+              <Text
+                className="song-title small-text no-bg"
+                size="2"
+                style={{ color: `${isFirstCard ? "#272727" : "#94a3b8"}` }}
+              >
                 {trackData.artists[0].name}
               </Text>
-              <Text className="song-title small-text no-bg" size="2">
+              <Text
+                className="song-title small-text no-bg"
+                size="2"
+                style={{ color: `${isFirstCard ? "#272727" : "#94a3b8"}` }}
+              >
                 •
               </Text>
-              <Text className="song-title small-text no-bg" size="2">
+              <Text
+                className="song-title small-text no-bg"
+                size="2"
+                style={{ color: `${isFirstCard ? "#272727" : "#94a3b8"}` }}
+              >
                 {trackData.album.release_date.split("-")[0]}
               </Text>
             </Flex>
