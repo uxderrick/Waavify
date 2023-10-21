4ce7dbad12e78d94292694c6295423433a96cc3e
Derrick Tsorme <49356701+uxderrick@users.noreply.github.com>
2023-10-21 11:18:41 +0000

    hm

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -23,12 +23,32 @@ export const takeScreenshot = (
     .then((canvas) => {
       // Convert the canvas to a data URL
       const image = canvas.toDataURL(fileType);
-      // console.log("Screenshot captured:", image);
 
-      const a = document.createElement("a");
-      a.href = image;
-      a.download = fileName;
-      a.click();
+      // Check if the data URL is empty (e.g. because of CORS restrictions)
+      if (canvas.toDataURL() === "data:,") {
+        console.error("The canvas is empty. Content may not have loaded.");
+        return;
+      }
+
+      //check if image has loaded before downloading
+      const img = new Image();
+      //set image source
+      img.src = image;
+      //load image
+      img.onload = () => {
+        console.log("Image loaded.");
+        // Create a link element
+        // Set the link's href to the data URL
+        // Set the link's download attribute to the filename
+        // Trigger the link element's click event
+        const a = document.createElement("a");
+        a.href = image;
+        a.download = fileName;
+        a.click();
+      };
+      img.onerror = () => {
+        console.error("Image failed to load.");
+      };
     })
     .catch((error) => {
       console.error("Error capturing screenshot:", error);
